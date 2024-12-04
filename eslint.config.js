6955e963875c9d60fce703263fe1b31ae5528004
NyiZinThant <nyizinthant@gmail.com>
2024-12-04 23:24:56 +0630

    add typescript and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact, { rules } from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact, { rules } from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  rules:{
-    "react/prop-types": "off",
+  {
+    rules: {
+      'react/prop-types': 'off',
+    },
   },
 ];
